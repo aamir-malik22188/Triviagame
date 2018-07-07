439589d197c5e388c44c3cda84fec78e2644b73e
Aamir Malik <aamir22188@aamirs-mbp.lan>
2018-07-06 23:55:47 -0500

    Incomplete Push

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Declare variables for questions
+
+var startButton
+var triviaQuestions = ["The use of canvas as a primary painting surface came of age in which century?", "Who painted 'The Scream'?", "English artist Andy Brown created a portrait of Queen Elizabeth II using what?", "How old is the earliest known human artwork?"]
+var triviaAnswers = [["14th", "16th", "18th" ], ["Vincent Van Gogh, Otto Meuller, Wassily Kandinsky, Edvard Munch"], ["Fish Bones", "Socks", "Tea Bags", "Bubblegum"], ["100,000 years old", "6,000 years old", "27,000 years old", "1.84 millions years old"]]
+var correctAnswer = false;
+var wrongAnswer = false;
+var gameTimer = 30;
+var countDownClock
+
+console.log(triviaQuestions);
+console.log(triviaAnswers);
+
